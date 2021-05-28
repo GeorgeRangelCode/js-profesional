90d2fd19543d4d397af2339cfecabdbe6fc3bdff
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-05-28 08:57:11 -0500

    Scope

---
+++ assets/MediaPlayer.js
@@ -0,0 +1,17 @@
+function MediaPlayer(config) {
+  this.media = config.el
+}
+
+MediaPlayer.prototype.play = function () {
+  this.media.play()
+}
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause()
+}
+
+MediaPlayer.prototype.tooglePlay = function () {
+  this.media.paused ? this.play() : this.pause()
+}
+
+export default MediaPlayer
--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,7 @@
+import MediaPlayer from "./MediaPlayer.js"
+
 const video = document.querySelector('video')
 const button = document.querySelector('button')
 
-function MediaPlayer(config) {
-  this.media = config.el
-}
-
-MediaPlayer.prototype.play = function () {
-  this.media.play()
-}
-
-MediaPlayer.prototype.pause = function () {
-  this.media.pause()
-}
-
-MediaPlayer.prototype.tooglePlay = function () {
-  this.media.paused ? this.play() : this.pause()
-}
-
 const player = new MediaPlayer({ el: video })
-
 button.onclick = () => player.tooglePlay()
