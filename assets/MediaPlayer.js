f68272411e3183347251e8c4a22585aa5b7bd21a
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-05-28 10:21:18 -0500

    El primer plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,6 +1,15 @@
 function MediaPlayer(config) {
   this.media = config.el
+  this.plugins = config.plugins || []
+
+  this._initPlugins()
 }
+
+  MediaPlayer.prototype._initPlugins = function () {
+    this.plugins.forEach(plugin => {
+      plugin.run(this)
+    });
+  }
 
 MediaPlayer.prototype.play = function () {
   this.media.play()
@@ -12,6 +21,14 @@ MediaPlayer.prototype.pause = function () {
 
 MediaPlayer.prototype.tooglePlay = function () {
   this.media.paused ? this.play() : this.pause()
+}
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true
+}
+
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false
 }
 
 export default MediaPlayer
--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
 import MediaPlayer from "./MediaPlayer.js"
+import AutoPlay from "./plugins/AutoPlay.js"
 
 const video = document.querySelector('video')
 const button = document.querySelector('button')
 
-const player = new MediaPlayer({ el: video })
+const player = new MediaPlayer({ el: video, plugins: [
+  new AutoPlay()
+] })
 button.onclick = () => player.tooglePlay()
